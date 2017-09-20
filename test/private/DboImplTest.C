2d53801ca12f88c9d7f421bd91f3509df02fbc68
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-20 07:41:51 +0100

    Patch: fix whitespace errors in ./test/private/DboImplTest.C

--- test/private/DboImplTest.C
+++ test/private/DboImplTest.C
@@ -58,24 +58,24 @@ BOOST_AUTO_TEST_CASE( DboImplTest_test1 )
   parseSql
     (SQL
      (WITH
-      regional_sales AS 
+      regional_sales AS
       (
-       SELECT region, SUM(amount) AS total_sales 
-       FROM orders 
-       GROUP BY region 
+       SELECT region, SUM(amount) AS total_sales
+       FROM orders
+       GROUP BY region
        ),
       top_regions AS
-      ( 
-       SELECT region 
-       FROM regional_sales 
-       WHERE total_sales > (SELECT SUM(total_sales)/10 FROM 
+      (
+       SELECT region
+       FROM regional_sales
+       WHERE total_sales > (SELECT SUM(total_sales)/10 FROM
 			    regional_sales)
       )
       SELECT region,
              product,
              SUM(quantity) AS product_units,
-             SUM(amount) AS product_sales 
-      FROM orders 
+             SUM(amount) AS product_sales
+      FROM orders
       WHERE region IN (SELECT region FROM top_regions)
       GROUP BY region, product, result, simpleSelectCount
       ),
