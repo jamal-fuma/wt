32cff7dd3e3c126afaf89b2c2977eff207c52fdf
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-20 07:41:48 +0100

    Patch: fix whitespace errors in ./src/Wt/Render/RenderUtils.C

--- src/Wt/Render/RenderUtils.C
+++ src/Wt/Render/RenderUtils.C
@@ -13,26 +13,26 @@ using namespace Wt::rapidxml;
 namespace Wt {
   namespace Render {
     namespace Utils {
-      bool normalizeWhitespace(Wt::Render::Block *block, 
+      bool normalizeWhitespace(Wt::Render::Block *block,
 			       Wt::rapidxml::xml_node<> *node,
-			       bool haveWhitespace, 
+			       bool haveWhitespace,
 			       Wt::rapidxml::xml_document<>& doc)
       {
 	Wt::rapidxml::memory_pool<>& pool = doc;
-      
+
 	char *v = node->value();
-      
+
 	unsigned len = node->value_size();
-      
+
 	std::string s;
 	s.reserve(len);
-      
+
 	for (unsigned i = 0; i < len; ++i) {
 	  if (block->isWhitespace(v[i])) {
 	    if (!haveWhitespace)
 	      s += ' ';
 	    haveWhitespace = true;
-	  } else if (i < len - 1 && 
+	  } else if (i < len - 1 &&
 		     v[i] == (char)0xC2 && v[i+1] == (char)0xA0) {
 	    /*
 	     * This wrong but will work temporarily. We are treating &nbsp;
@@ -47,7 +47,7 @@ namespace Wt {
 	    haveWhitespace = false;
 	  }
 	}
-      
+
 	char *nv = pool.allocate_string(s.c_str(), s.length());
 	node->value(nv, s.length());
 
@@ -59,8 +59,8 @@ namespace Wt {
 	return node->type() == node_element;
       }
 
-      void fetchBlockChildren(xml_node<> *node, 
-			      Wt::Render::Block *block, 
+      void fetchBlockChildren(xml_node<> *node,
+			      Wt::Render::Block *block,
 			      std::vector<Wt::Render::Block *> &children)
       {
 	for (xml_node<> *child = node->first_node(); child;
