c82e5e3d80a8911b639f32ba8ced2eb44f4f9c23
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-20 07:41:08 +0100

    Patch: fix whitespace errors in ./src/Wt/WSslInfo.C

--- src/Wt/WSslInfo.C
+++ src/Wt/WSslInfo.C
@@ -33,18 +33,18 @@ std::string WSslInfo::gdb() const
   std::stringstream ss;
   ss << "client certificate:\n";
   clientCertificate_.gdb();
-  
+
   for (unsigned i = 0; i < clientCertificateChain_.size(); ++i) {
     ss << "client cert chain " << i << " :\n";
     clientCertificateChain_[i].gdb();
   }
 
   ss
-    << "valid: " 
+    << "valid: "
     << (clientVerificationResult_.state() == ValidationState::Valid)
     << std::endl
     << "validity info: " << clientVerificationResult_.message() << std::endl;
-  
+
   return ss.str();
 }
 
