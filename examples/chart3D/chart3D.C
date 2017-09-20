7f92b34a1e3821c51fd6928c97f19fc0d9ae0da1
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-09-20 07:41:52 +0100

    Patch: fix whitespace errors in ./examples/chart3D/chart3D.C

--- examples/chart3D/chart3D.C
+++ examples/chart3D/chart3D.C
@@ -43,12 +43,12 @@ TestApp::TestApp(const Wt::WEnvironment& env)
   chartExPicker_->addItem("Categorical Data");
   chartExPicker_->addItem("Colormap Example");
   chartExPicker_->changed().connect(this, &TestApp::switchExamples);
-  
+
   stack_ = wrapper->addWidget(Wt::cpp14::make_unique<Wt::WStackedWidget>());
   numEx_ = stack_->addWidget(Wt::cpp14::make_unique<NumericalExample>());
   categoryEx_ = stack_->addWidget(Wt::cpp14::make_unique<CategoryExample>());
   colormap_ = stack_->addWidget(Wt::cpp14::make_unique<ColorMapTest>());
-  
+
   chartExPicker_->setCurrentIndex(0);
   stack_->setCurrentWidget(numEx_);
 }
